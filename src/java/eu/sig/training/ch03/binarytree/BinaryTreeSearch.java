3b1ddf21185bb868a4a46c9731146a8245405fb8
aliyafathma <aliya.fathma13@gmail.com>
2019-05-29 16:43:13 +0700

    make the codes in binaryTree shorter

--- src/java/eu/sig/training/ch03/binarytree/BinaryTreeSearch.java
+++ src/java/eu/sig/training/ch03/binarytree/BinaryTreeSearch.java
@@ -5,26 +5,15 @@ public class BinaryTreeSearch {
     // tag::calculateDepth[]
     public static int calculateDepth(BinaryTreeNode<Integer> node, int nodeValue) {
         int depth = 0;
-        if (node.getValue() == nodeValue) {
-            return depth;
+        if (node.getValue() == nodeValue) return depth;
+        if (nodeValue < node.getValue() && node.getLeft() != null) {
+            return 1 + calculateDepth(left, nodeValue);
+        }
+        if(nodeValue > node.getValue() && node.getRight() != null){
+            return 1 + calculateDepth(right, nodeValue);
         } else {
-            if (nodeValue < node.getValue()) {
-                BinaryTreeNode<Integer> left = node.getLeft();
-                if (left == null) {
-                    throw new TreeException("Value not found in tree!");
-                } else {
-                    return 1 + calculateDepth(left, nodeValue);
-                }
-            } else {
-                BinaryTreeNode<Integer> right = node.getRight();
-                if (right == null) {
-                    throw new TreeException("Value not found in tree!");
-                } else {
-                    return 1 + calculateDepth(right, nodeValue);
-                }
-            }
+            throw new TreeException("Value not found in tree!");
         }
     }
-    // end::calculateDepth[]
-
 }
+    // end::calculateDepth[]
